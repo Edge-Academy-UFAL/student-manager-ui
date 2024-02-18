45c2219ab7ce3b54946ef002fbfb0f15c4b79f61
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-02-18 00:02:55 -0300

    feat : add styling to header + user profile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,17 @@
 'use client'
 import { Separator } from '../ui/separator'
-import { Cat } from 'lucide-react'
+import { Cat, Users } from 'lucide-react'
 import { NavLink } from './nav-link'
+import { UserSheet } from './user-sheet'
 import { ModeToggle } from '../theme-toggle'
 import { useRouter } from 'next/navigation'
 
 // sample routes
 export const routes = [
   {
-    name: 'Route 1',
-    path: '/',
-    icon: <Cat className="h-4 w-4" />,
+    name: 'Alunos',
+    path: '/alunos',
+    icon: <Users className="h-4 w-4" />,
   },
   {
     name: 'Route 2',
@@ -42,13 +43,13 @@ const Header = () => {
   })
 
   return (
-    <header className="flex h-16 items-center justify-between border-b dark:border-[#29292c] bg-white px-6 dark:bg-[#111113]">
+    <header className="flex h-16 items-center justify-between border-b px-6">
       <div className="flex items-center gap-4 lg:gap-6">
         <div
           className="flex gap-4 hover:cursor-pointer"
           onClick={() => router.push('/')}
         >
-          <h1 className="flex items-center gap-1.5 text-base transition-colors font-medium text-gray-950 hover:text-gray-800 dark:text-white">
+          <h1 className="flex items-center gap-1.5 text-base transition-colors font-medium bg-gradient-to-r p-[0.35rem] rounded-lg from-blue-600 via-green-500 to-indigo-400 text-white shadow-sm">
             Edge Academy
           </h1>
         </div>
@@ -56,8 +57,12 @@ const Header = () => {
         <nav className="flex items-center gap-4 lg:gap-6">{navLinks}</nav>
       </div>
       <div className="flex items-center gap-4 lg:gap-6 ">
-        <Separator orientation="vertical" className="h-6 dark:bg-[#2d2d37]" />
         <ModeToggle />
+        <Separator
+          orientation="vertical"
+          className="h-6 dark:bg-[#2d2d37]"
+        />{' '}
+        <UserSheet />
       </div>
     </header>
   )
--- src/components/header/nav-link.tsx
+++ src/components/header/nav-link.tsx
@@ -14,8 +14,7 @@ export function NavLink(props: NavLinkProps) {
     <Link href={props.to}>
       <h1
         data-current={pathname === props.to} // add data-current attribute to the NavLink component to indicate the current route
-        className="flex items-center gap-1.5 text-[0.9rem] font-medium text-gray-500 hover:text-gray-900 
-        dark:text-white dark:hover:text-gray-300 transition-colors duration-200 ease-in-out cursor-pointer"
+        className="flex items-center gap-1.5 text-[0.9rem] font-medium hover:text-muted-foreground data-[current=true]:text-muted-foreground"
       >
         {props.children}
       </h1>
