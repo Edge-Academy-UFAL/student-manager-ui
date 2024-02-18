4e8c13bb04e2a3ba482bb0fb910bf36ba33fc4f6
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-02-18 18:59:44 -0300

    .

--- src/app/alunos/page.tsx
+++ src/app/alunos/page.tsx
@@ -1,28 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import DataTableDemo from '@/components/table'
-import { notFound } from 'next/navigation'
+import { tableData } from '@/lib/utils'
 
-const getData = async () => {
-  const res = await fetch(`http://localhost:3000/api/students`, {
-    cache: 'no-store', // o ideal é que tenha cache pois os dados não mudam constantemente mas por enquanto vou deixar sem cache
-  })
+// const getData = async () => {
+//   const res = await fetch(`http://localhost:3000/api/students`, {
+//     cache: 'no-store', // o ideal é que tenha cache pois os dados não mudam constantemente mas por enquanto vou deixar sem cache
+//   })
 
-  if (!res.ok) {
-    return null
-  }
+//   if (!res.ok) {
+//     return null
+//   }
 
-  return res.json()
-}
+//   return res.json()
+// }
 
 const StudentSearchPage = async () => {
-  const data = await getData()
+  // const data = await getData()
 
-  if (!data) {
-    return notFound()
-  }
+  // if (!data) {
+  //   return notFound()
+  // }
 
   return (
     <div>
-      <DataTableDemo data={data.tableData} />
+      <DataTableDemo data={tableData} />
     </div>
   )
 }
