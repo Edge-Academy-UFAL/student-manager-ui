f0f09f6b5e44999cbf913528586a775df1065410
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-02-18 13:46:36 -0300

    feat : add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,12 +2,15 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="bg-gray-100">
+    <div className="">
       <div className="flex h-[calc(100vh-70px)] flex-col items-center justify-center">
-        <h1 className="text-8xl font-bold text-gray-800">404</h1>
-        <p className="text-4xl font-medium text-gray-800">Page Not Found</p>
-        <Link href="/chat" className="mt-4 text-xl hover:underline">
-          Go back home
+        <h1 className="text-8xl font-bold">404</h1>
+        <p className="text-4xl font-medium">Page Not Found</p>
+        <Link
+          href="/"
+          className="mt-4 text-xl p-3  text-white rounded-lg bg-gradient-to-tr from-[#00cdac] to-[#8ac926] shadow"
+        >
+          Ir para o inicio
         </Link>
       </div>
     </div>
