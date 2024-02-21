686b3037acb31f8b3bf97d8e8c8948ff727a0454
Arthurpmrs <arthurpmrs@gmail.com>
2024-02-21 20:28:02 -0300

    feat: add student registration dialog

--- src/components/student-register-dialog.tsx
+++ src/components/student-register-dialog.tsx
@@ -19,121 +19,19 @@ import { PlusIcon } from '@radix-ui/react-icons'
 import { useState } from "react"
 import { Badge } from "@/components/ui/badge"
 
-function ContentInputDialog(
-    props: {
-        open: boolean,
-        onOpenChange: (open: boolean) => void,
-        studentEmails: string,
-        handleChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
-        handleSubmit: () => void
+function validateEmails(emails: string): Array<{ email: string, isValid: Boolean }> {
+    const emailArray = emails.split(',').map((email) : string => email.trim())
+    const emailValidation = []
+    for (let email of emailArray) {
+        let isValid = true
+        if (!email.includes('@edge.ufal.br')) {
+            isValid = false
+        }
+        const obj = { email: email, isValid: isValid }
+        emailValidation.push(obj)
     }
-
-) {
-    function handleOpen() {
-        props.onOpenChange(true)
-    }
-
-    return (
-        <Dialog open={props.open} onOpenChange={props.onOpenChange}>
-            <DialogTrigger asChild>
-                <Button variant="default" onClick={handleOpen}>
-                    <PlusIcon />
-                    <span className='ml-2'>Adicionar alunos</span>
-                </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-lg h-1000">
-                <DialogHeader>
-                    <DialogTitle>Register new students</DialogTitle>
-                    <DialogDescription>
-                        Add new students to the Edge Academy program. Insert below the
-                        "@edge.ufal.br" email addresses of the students you want to add
-                        separated by commas.
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <Label htmlFor="student-emails">E-mails</Label>
-                    <Textarea className="h-[160px]"
-                        placeholder="Type the student e-mails here, separated by commas."
-                        id="student-emails"
-                        value={props.studentEmails}
-                        onChange={props.handleChange}
-                    />
-                </div>
-                <DialogFooter>
-                    <Button type="submit" onClick={props.handleSubmit}>Submit</Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
+    return emailValidation
 }
-
-function ConfirmationDialog(props: {
-    open: boolean,
-    onOpenChange: (open: boolean) => void,
-    handleGoBack: () => void,
-    handleConfirm: () => void
-}) {
-    return (
-        <Dialog open={props.open} onOpenChange={props.onOpenChange}>
-            <DialogContent className="sm:max-w-lg h-1000">
-                <DialogHeader>
-                    <DialogTitle>Você tem certeza?</DialogTitle>
-                    <DialogDescription>
-                        Add new students to the Edge Academy program. Insert below the
-                        "@edge.ufal.br" email addresses of the students you want to add
-                        separated by commas.
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <span>lalalala</span>
-                </div>
-                <DialogFooter>
-                    <Button type="submit" onClick={props.handleGoBack}>Go back</Button>
-                    <Button type="submit">Confirm</Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
-}
-
-// function ConfirmationDialog(
-//     props: {
-//         emails: Array<{ email: string, isValid: Boolean }>,
-//         handleConfirm: () => void,
-//         handleGoBack: () => void
-//     }
-// ) {
-//     return (
-//         <Dialog>
-//             <DialogTrigger asChild>
-//                 <Button variant="default">
-//                     <PlusIcon />
-//                     <span className='ml-2'>Adicionar alunos</span>
-//                 </Button>
-//             </DialogTrigger>
-//             <DialogContent className="sm:max-w-lg h-1000">
-//                 <DialogHeader>
-//                     <DialogTitle>Register new students</DialogTitle>
-//                     <DialogDescription>
-//                         Add new students to the Edge Academy program. Insert below the
-//                         "@edge.ufal.br" email addresses of the students you want to add
-//                         separated by commas.
-//                     </DialogDescription>
-//                 </DialogHeader>
-//                 <div className="grid gap-4 py-4">
-//                     {props.emails.map((email, index) => (
-//                         <Badge>{email.email}</Badge>
-//                     ))}
-//                 </div>
-//                 <DialogFooter>
-//                     <Button type="submit" onClick={props.handleGoBack}>Go back</Button>
-//                     <Button type="submit" onClick={props.handleConfirm}>Confirm</Button>
-//                 </DialogFooter>
-//             </DialogContent>
-//         </Dialog>
-
-//     )
-// }
 
 function ConfirmationDialogContent(props: {
     validatedEmails: Array<{ email: string, isValid: Boolean }>,
@@ -143,9 +41,10 @@ function ConfirmationDialogContent(props: {
     return (
         <>
             <DialogHeader>
-                <DialogTitle>Verify the e-mails!</DialogTitle>
+                <DialogTitle>Are you sure?</DialogTitle>
                 <DialogDescription>
-                    These are the e-mails typed. Please verify if they are correct.
+                    These are the e-mails typed. Please verify if they are correct before submiting.
+                    Invalid e-mails will not be registered.
                 </DialogDescription>
             </DialogHeader>
             <div className="grid gap-4 py-4">
@@ -156,7 +55,7 @@ function ConfirmationDialogContent(props: {
                             ?
                             <Badge className="w-full">{email.email}</Badge>
                             :
-                            <Badge variant="destructive">{email.email}</Badge>
+                            <Badge className="w-full" variant="destructive">{email.email}</Badge>
                     ))}
                 </ScrollArea>
             </div>
@@ -170,7 +69,7 @@ function ConfirmationDialogContent(props: {
 }
 
 function InputDialogContent(props: {
-    studentEmails: string,
+    emails: string,
     handleTextAreaChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
     handleSubmit: () => void
 
@@ -190,7 +89,7 @@ function InputDialogContent(props: {
                 <Textarea className="h-[160px]"
                     placeholder="Type the student e-mails here, separated by commas."
                     id="student-emails"
-                    value={props.studentEmails}
+                    value={props.emails}
                     onChange={props.handleTextAreaChange}
                 />
             </div>
@@ -207,88 +106,67 @@ function InputDialogContent(props: {
 
 export function StudentRegisterDialog() {
 
-    const [studentEmails, setStudentEmails] = useState<string>("")
-    const [showInputDialog, setShowInputDialog] = useState<boolean>(false)
-    const [showConfirmationDialog, setShowConfirmationDialog] = useState<boolean>(false)
+    const [emails, setEmails] = useState<string>("")
+    const [showDialog, setShowDialog] = useState<boolean>(false)
+    const [showConfirmation, setShowConfirmation] = useState<boolean>(false)
     const [validatedEmails, setValidatedEmails] = useState<Array<{ email: string, isValid: Boolean }>>([])
 
+    function handleDialogOpen(): void {
+        // This is necessary to always open the dialog on the input page.
+        setShowConfirmation(false)
+        setShowDialog(true)
+    }
+
+    function onShowDialogChange(): void {
+        // This is necessary, because when the Dialog is closed showDialog
+        // is false. So, everytime showDialog tried to change to true, this
+        // would trigger and the Dialog would never open.
+        showDialog && setShowDialog(false)
+    }
+
     function handleTextAreaChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        // setError(noErrors);
-        const { name, value } = event.target;
-        setStudentEmails(value)
-        // setUserData({ ...userData, [name]: value })
+        setEmails(event.target.value)
     }
 
-    function validateEmails(emails: string): Array<{ email: string, isValid: Boolean }> {
-        const emailArray = emails.split(',')
-        const emailValidation = []
-        for (let email of emailArray) {
-            let isValid = true
-            if (!email.includes('@edge.ufal.br')) {
-                isValid = false
-            }
-            const obj = { email: email, isValid: isValid }
-            emailValidation.push(obj)
-        }
-        console.log(emailValidation)
-        return emailValidation
-    }
-
-    function handleSubmit() {
-        console.log(studentEmails)
-        // setShowInputDialog(false)
-        const emailValidation = validateEmails(studentEmails)
-        setValidatedEmails(emailValidation)
-        setShowConfirmationDialog(true)
-    }
-
-    function handleConfirm(): void {
+    function handleSubmit() :void {
+        setValidatedEmails(validateEmails(emails))
+        setShowConfirmation(true)
     }
 
     function handleGoBack(): void {
-        // setShowInputDialog(true)
-        setShowConfirmationDialog(false)
-        console.log("haahah")
+        setShowConfirmation(false)
     }
 
-    function handleDialogClose(): void {
-        // setShowConfirmationDialog(false)
-        if (showInputDialog === true) {
-            setShowInputDialog(false)
-        }
-    }
-
-    function handleOpen(): void {
-        setShowInputDialog(true)
-        setShowConfirmationDialog(false)
+    function handleConfirm(): void {
+        console.log("enviei para o backend")
+        console.log(validatedEmails)
     }
 
     return (
-        <Dialog open={showInputDialog} onOpenChange={handleDialogClose}>
+        <Dialog open={showDialog} onOpenChange={onShowDialogChange}>
             <DialogTrigger asChild>
-                <Button variant="default" onClick={handleOpen}>
+                <Button variant="default" onClick={handleDialogOpen}>
                     <PlusIcon />
                     <span className='ml-2'>Adicionar alunos</span>
                 </Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-lg">
                 {
-                    showConfirmationDialog ?
-                        <ConfirmationDialogContent
-                            validatedEmails={validatedEmails}
-                            handleConfirm={handleConfirm}
-                            handleGoBack={handleGoBack}
-                        />
-                        :
-                        <InputDialogContent
-                            studentEmails={studentEmails}
-                            handleTextAreaChange={handleTextAreaChange}
-                            handleSubmit={handleSubmit}
-                        />
-
+                    showConfirmation 
+                    ?
+                    <ConfirmationDialogContent
+                        validatedEmails={validatedEmails}
+                        handleConfirm={handleConfirm}
+                        handleGoBack={handleGoBack}
+                    />
+                    :
+                    <InputDialogContent
+                        emails={emails}
+                        handleTextAreaChange={handleTextAreaChange}
+                        handleSubmit={handleSubmit}
+                    />
                 }
             </DialogContent>
-
         </Dialog>
     )
 }
