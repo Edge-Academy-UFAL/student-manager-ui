3f31536b59883d5c715ade428f26c2006c8b6bfd
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-02-23 18:18:03 -0300

    feat : implement new error component

--- src/app/(dashboard)/alunos/page.tsx
+++ src/app/(dashboard)/alunos/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import DataTableDemo from '@/components/table'
-import { notFound } from 'next/navigation'
 
 const prodURL = `https://json-server-edge-academy.vercel.app/students`
 const devURL = `http://localhost:3333/students`
@@ -8,25 +7,28 @@ const devURL = `http://localhost:3333/students`
 // se quiser testar com dados no localhost basta mudar a url para "devURL" e rodar "pnpm json-server server.json -w -p 3333" no terminal
 // lembrar de trocar de volta para a url do "prodURL" antes de fazer o commit para não dar erro no build
 const getData = async () => {
-  const res = await fetch(prodURL, {
-    // next: {
-    //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
-    // },
-    cache: 'no-store',
-  })
+  try {
+    const res = await fetch(prodURL, {
+      // next: {
+      //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
+      // },
+      cache: 'no-store',
+    })
 
-  if (!res.ok) {
-    return null
+    if (!res.ok) {
+      return null
+    }
+    return res.json()
+  } catch (error) {
+    throw new Error('Erro de conexão com o servidor')
   }
-
-  return res.json()
 }
 
 const StudentSearchPage = async () => {
   const data = await getData()
 
   if (!data) {
-    return notFound()
+    throw new Error('Erro ao buscar os dados')
   }
 
   return (
--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -8,18 +8,22 @@ const devURL = `http://localhost:3333/profile-settings`
 // se quiser testar com dados no localhost basta mudar a url para "devURL" e rodar "pnpm json-server server.json -w -p 3333" no terminal
 // lembrar de trocar de volta para a url do "prodURL" antes de fazer o commit para não dar erro no build
 const getData = async () => {
-  const res = await fetch(prodURL, {
-    // next: {
-    //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
-    // },
-    cache: 'no-store',
-  })
+  try {
+    const res = await fetch(prodURL, {
+      // next: {
+      //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
+      // },
+      cache: 'no-store',
+    })
 
-  if (!res.ok) {
-    return null
+    if (!res.ok) {
+      throw new Error('Erro ao buscar os dados')
+    }
+
+    return res.json()
+  } catch (error) {
+    throw new Error('Erro de conexão com o servidor')
   }
-
-  return res.json()
 }
 
 export default async function SettingsProfilePage() {
