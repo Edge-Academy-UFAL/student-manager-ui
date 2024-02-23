1130aa2104f716ec6df43e8bc21ee7e3231d63a1
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-02-22 23:09:48 -0300

    fix: adjust mobile support in settings

--- src/app/(dashboard)/settings/layout.tsx
+++ src/app/(dashboard)/settings/layout.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
 
 import { Separator } from '@/components/ui/separator'
 import { SidebarNav } from './components/sidebar-nav'
@@ -35,23 +34,7 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+      <div className="space-y-6 p-10 pb-16">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Configurações</h2>
           <p className="text-muted-foreground">
@@ -64,7 +47,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
           <aside className="-mx-4 lg:w-1/5">
             <SidebarNav items={sidebarNavItems} />
           </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
+          <div className="flex-1 lg:max-w-3xl">{children}</div>
         </div>
       </div>
     </>
