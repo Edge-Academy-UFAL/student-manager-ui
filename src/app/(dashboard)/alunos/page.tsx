8e917aed12618a582f745f6879927442c1b65010
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-02-23 16:48:04 -0300

    feat : add production and development URLs

--- src/app/(dashboard)/alunos/page.tsx
+++ src/app/(dashboard)/alunos/page.tsx
@@ -2,16 +2,18 @@
 import DataTableDemo from '@/components/table'
 import { notFound } from 'next/navigation'
 
+const prodURL = `https://json-server-edge-academy.vercel.app/students`
+const devURL = `http://localhost:3333/students`
+
+// se quiser testar com dados no localhost basta mudar a url para "devURL" e rodar "pnpm json-server server.json -w -p 3333" no terminal
+// lembrar de trocar de volta para a url do "prodURL" antes de fazer o commit para não dar erro no build
 const getData = async () => {
-  const res = await fetch(
-    `https://json-server-edge-academy.vercel.app/students`,
-    {
-      // next: {
-      //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
-      // },
-      cache: 'no-store',
-    },
-  )
+  const res = await fetch(prodURL, {
+    // next: {
+    //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
+    // },
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
     return null
--- src/app/(dashboard)/settings/appearance/page.tsx
+++ src/app/(dashboard)/settings/appearance/page.tsx
@@ -1,16 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Separator } from '@/components/ui/separator'
 import { AppearanceForm } from './appearance-form'
 
+const prodURL = `https://json-server-edge-academy.vercel.app/appearance`
+const devURL = `http://localhost:3333/appearance`
+
+// se quiser testar com dados no localhost basta mudar a url para "devURL" e rodar "pnpm json-server server.json -w -p 3333" no terminal
+// lembrar de trocar de volta para a url do "prodURL" antes de fazer o commit para não dar erro no build
 const getData = async () => {
-  const res = await fetch(
-    `https://json-server-edge-academy.vercel.app/appearance`,
-    {
-      // next: {
-      //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
-      // },
-      cache: 'no-store',
-    },
-  )
+  const res = await fetch(prodURL, {
+    // next: {
+    //   revalidate: 15, // dessa forma, a cada 15 segundos a página será atualizada
+    // },
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
     return null
