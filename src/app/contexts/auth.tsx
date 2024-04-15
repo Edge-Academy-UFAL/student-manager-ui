2a1eaa2c9c995ce2ffa447ea38935c00c4f9dade
Davi Romão <davi.romao0210@gmail.com>
2024-04-15 14:49:23 -0300

    feat: create auth context

---
+++ src/app/contexts/auth.tsx
@@ -0,0 +1,87 @@
+'use client'
+import { setCookie, deleteCookie, getCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
+import { createContext, useContext, useEffect, useState } from 'react'
+
+interface AuthContextType {
+  // TODO: type of user
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  user: any
+  isAuthenticated: boolean
+  login: (email: string, password: string) => Promise<number>
+  logout: () => void
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    async function loadUserFromCookies() {
+      const token = getCookie('token')
+      if (token) {
+        const user = await fetch('http://127.0.0.1:8080/api/v1/auth/me', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((res) => res.json())
+        setUser(user)
+      }
+    }
+    loadUserFromCookies()
+  }, [])
+
+  const login = async (email: string, password: string) => {
+    const res = await fetch('http://127.0.0.1:8080/api/v1/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+
+    if (res.ok) {
+      const data = await res.json()
+      const oneDay = 1000 * 60 * 60 * 24
+
+      setCookie('token', data.token, {
+        expires: new Date(Date.now() + oneDay),
+      })
+
+      const user = await fetch('http://127.0.0.1:8080/api/v1/auth/me', {
+        headers: {
+          Authorization: `Bearer ${data.token}`,
+        },
+      }).then((res) => res.json())
+
+      setUser(user)
+    }
+
+    return res.status
+  }
+
+  const logout = () => {
+    deleteCookie('token')
+    setUser(null)
+
+    router.push('/login')
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated: !!user, user, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
