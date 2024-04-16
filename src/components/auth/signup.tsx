5c3e9cb898d2605ab46115d96fe3f61e32456d57
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-04-16 20:38:58 -0300

    feat: add sign up page with field validations

--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -11,14 +12,54 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Upload } from 'lucide-react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useToast } from '../ui/use-toast'
+import { useForm } from 'react-hook-form'
+
 import ReturnButton from './return-button'
+import SelectCourse from './select-couse'
+import UploadProfilePic from './uploadProfilePic'
+
+import { RegisterSchema } from '@/lib/schemas'
+
+import { Eye, EyeOff } from 'lucide-react'
 
 export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(RegisterSchema),
+  })
+
+  const { toast } = useToast()
+  // const { push } = useRouter()
+
+  const [course, setCourse] = useState('Ciência da Computação')
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  const submitHandler = (data: RegisterSchema) => {
+    console.log(data)
+    toast({
+      title: 'Você enviou os seguintes valores:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
   return (
-    <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden">
+    <div className="flex flex-col justify-center items-center min-h-screen overflow-hidden">
       <ReturnButton />
-      <div className="w-full m-auto lg:max-w-lg">
+      <form
+        className="w-full m-auto lg:max-w-4xl"
+        onSubmit={handleSubmit(submitHandler)}
+      >
         <Card>
           <CardHeader className="space-y-1">
             <CardTitle className="text-2xl text-center">
@@ -28,29 +69,134 @@ export default function SignUp() {
               Preencha os campos abaixo para completar seu cadastro
             </CardDescription>
           </CardHeader>
-          <CardContent className="grid gap-3">
-            <div className="grid gap-3">
-              <Label htmlFor="password">Senha</Label>
-              <Input id="password" type="password" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="password">Adicionar foto</Label>
-              <div className="flex flex-col items-center space-y-3 hover:cursor-pointer">
-                <div className="w-full  p-12 border-dashed border-2 border-gray-200 dark:border-gray-700 rounded-lg flex flex-col items-center gap-2 text-gray-500 dark:text-gray-400">
-                  <Upload className="w-10 h-10" />
-                  <span className="text-xs text-center">
-                    Arraste e solte o arquivo aqui ou clique para selecionar
-                  </span>
-                </div>
+          <CardContent className="flex flex-col gap-3">
+            <div className="md:grid md:grid-cols-2 gap-5">
+              <div className="grid gap-3">
+                <Label>Nome Completo*</Label>
+                <Input {...register('name')} />
+                <p className="text-red-500 text-xs">{errors?.name?.message}</p>
               </div>
-            </div>
-          </CardContent>
+              <div className="grid gap-3">
+                <Label>Curso*</Label>
+                <SelectCourse course={course} setCourse={setCourse}>
+                  <Input {...register('course')} value={course} />
+                </SelectCourse>
+                <p className="text-red-500 text-xs">
+                  {errors?.course?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Data de Nascimento*</Label>
+                <Input type="date" {...register('dateOfBirth')} />
+                <p className="text-red-500 text-xs">
+                  {errors?.dateOfBirth?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Número de matricula*</Label>
+                <Input {...register('registrationNumber')} />
+                <p className="text-red-500 text-xs">
+                  {errors?.registrationNumber?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Telefone*</Label>
+                <Input
+                  {...register('phoneNumber')}
+                  placeholder="(99) 9999-9999"
+                  pattern="(\([0-9]{2}\))\s([9]{1})([0-9]{4})-([0-9]{4})"
+                />
 
+                <p className="text-red-500 text-xs">
+                  {errors?.phoneNumber?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Período*</Label>
+                <Input
+                  type="number"
+                  min="1"
+                  max="8"
+                  {...register('semester', {
+                    setValueAs: (v) => (v === '' ? undefined : parseInt(v, 10)),
+                  })}
+                />
+                <p className="text-red-500 text-xs">
+                  {errors?.semester?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Telefone Secundário</Label>
+                <Input
+                  {...register('secondaryPhoneNumber')}
+                  placeholder="(99) 9999-9999"
+                  pattern="(\([0-9]{2}\))\s([9]{1})([0-9]{4})-([0-9]{4})"
+                />
+                <p className="text-red-500 text-xs">
+                  {errors?.secondaryPhoneNumber?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Período de Ingresso*</Label>
+                <Input
+                  {...register('entrySemester')}
+                  placeholder="Ex: 2021.1, 2022.2"
+                />
+                <p className="text-red-500 text-xs">
+                  {errors?.entrySemester?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Senha*</Label>
+                <div className="relative">
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    {...register('password')}
+                  />
+                  <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <Eye size={15} /> : <EyeOff size={15} />}
+                  </button>
+                </div>
+                <p className="text-red-500 text-xs">
+                  {errors?.password?.message}
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label>Confirmar senha*</Label>
+                <div className="relative">
+                  <Input
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    {...register('confirmPassword')}
+                  />
+                  <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  >
+                    {showConfirmPassword ? (
+                      <Eye size={15} />
+                    ) : (
+                      <EyeOff size={15} />
+                    )}
+                  </button>
+                </div>
+                <p className="text-red-500 text-xs">
+                  {errors?.confirmPassword?.message}
+                </p>
+              </div>{' '}
+            </div>
+            {/* Ainda falta fazer o upload de foto */}
+            <UploadProfilePic />
+          </CardContent>
           <CardFooter>
             <Button className="w-full">Cadastrar</Button>
           </CardFooter>
         </Card>
-      </div>
+      </form>
     </div>
   )
 }
---
+++ src/lib/schemas.ts
@@ -0,0 +1,62 @@
+import { z } from 'zod'
+
+export const RegisterSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, { message: 'A senha deve possuir mais de 8 caracteres.' })
+      .max(20, { message: 'A senha deve possuir menos de 20 caracteres.' })
+      .refine(
+        (value) =>
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,20}$/.test(value),
+        {
+          message:
+            'A senha deve conter pelo menos uma letra minúscula, uma maiúscula e um dígito.',
+        },
+      ),
+    confirmPassword: z.string(),
+
+    semester: z
+      .number()
+      .min(1, 'Semestre Inválido.')
+      .max(8, 'Semestre Inválido.'),
+    entrySemester: z.string().refine(
+      (value) => {
+        const currentYear = new Date().getFullYear()
+        const entryYear = parseInt(value.split('.')[0], 10)
+        return /^\d{4}\.(1|2)$/.test(value) && entryYear <= currentYear
+      },
+      {
+        message:
+          'O ano do período de ingresso não pode ser posterior ao ano atual e deve estar no formato ANO.SEMESTRE_LETIVO (ex: 2020.1, 2020.2, 2024.1, 2024.2)',
+      },
+    ),
+    phoneNumber: z
+      .string()
+      .min(10, 'Número de telefone inválido.')
+      .max(15, 'Número de telefone inválido.'),
+    secondaryPhoneNumber: z
+      .string()
+      .min(10, 'Número de telefone inválido.')
+      .max(15, 'Número de telefone inválido.')
+      .optional()
+      .or(z.literal('')),
+    name: z
+      .string()
+      .min(3, 'Preencha com seu nome completo.')
+      .max(30, 'Limite de 30 caracteres atingido')
+      .regex(/^[a-zA-Z]+$/, 'O nome deve conter apenas letras A-Z a-z.'),
+    course: z.enum(['Ciência da Computação', 'Engenharia de Computação']),
+    registrationNumber: z.string().refine((value) => /^\d{8}$/.test(value), {
+      message: 'Formato de matrícula inválido',
+    }),
+    dateOfBirth: z
+      .string()
+      .min(1, { message: 'Insira sua data de nascimento.' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'As senhas não coincidem',
+  })
+
+export type RegisterSchema = z.infer<typeof RegisterSchema>
