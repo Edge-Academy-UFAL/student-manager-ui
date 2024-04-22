a38adfb6d0c6981710857764953e533a8aa1b1bd
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-04-22 11:29:23 -0300

    feat: add ptBR calendar and set default year

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -45,6 +45,7 @@ import {
 
 import { Calendar } from '@/components/ui/calendar'
 import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { cn } from '@/lib/utils'
 
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -209,6 +210,8 @@ const SignUpForm = ({ id }: { id: string }) => {
                       </PopoverTrigger>
                       <PopoverContent className="w-auto p-0" align="start">
                         <Calendar
+                          locale={ptBR}
+                          defaultMonth={new Date(2003, 6)}
                           mode="single"
                           selected={field.value}
                           onSelect={field.onChange}
