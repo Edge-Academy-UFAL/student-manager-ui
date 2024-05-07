979fdc33136c890c78113c7573eb3006888bc2fb
Davi Romão <davi.romao0210@gmail.com>
2024-05-07 20:09:10 -0300

    fix: remove dummy data

--- src/components/student-profile/student-profile.tsx
+++ src/components/student-profile/student-profile.tsx
@@ -43,9 +43,7 @@ const StudentProfile = ({ username }: { username: string }) => {
       if (response.ok) {
         console.log('response', response)
         const studentInfos = await response.json()
-        studentInfos.about =
-          studentInfos.about ||
-          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu'
+        studentInfos.about = studentInfos.about || 'Não fornecido!'
         setStudentData(studentInfos)
         setLoading(false)
       }
