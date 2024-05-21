49733b96d7784b4b9969ca36899293e9dd5ba0f1
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-05-21 15:27:49 -0300

    feat : add add new activity

---
+++ src/lib/functions/http/add-activity-req.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use server'
+
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth/next'
+import { revalidateTag } from 'next/cache'
+
+export const addActivity = async (data: any) => {
+  'use server'
+
+  const session = await getServerSession(authOptions)
+  const token = session?.user.authToken
+
+  try {
+    const res = await fetch(`${process.env.backendRoute}/api/v1/activities`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+
+    if (!res.ok) {
+      throw new Error('Erro ao adicionar atividade')
+    }
+
+    revalidateTag('user-data')
+
+    return true
+  } catch (error) {
+    return false
+  }
+}
