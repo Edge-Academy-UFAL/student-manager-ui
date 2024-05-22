ac36fd89fe2984bc7a8fdfa6c05e535df039b55e
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-05-22 00:40:33 -0300

    feat: adding a better ui to the upload of the record

--- src/components/student-record/student-record-dialog.tsx
+++ src/components/student-record/student-record-dialog.tsx
@@ -17,14 +17,18 @@ import { Upload } from 'lucide-react'
 import { useState } from 'react'
 import { revalidateRecordPage } from '@/app/actions'
 import { getUsername } from '@/lib/utils'
+import { useToast } from "@/components/ui/use-toast"
 
-const StudentRecordDialog = () => {
+const StudentRecordDialog = ({ pdfViewerKey, setPdfViewerKey } : {pdfViewerKey: number, setPdfViewerKey: (src: number) => void}) => {
 	const { data } = useSession()
 
 	const [selectedRecord, setSelectedRecord] = useState<File | null>()
 	const [canSaveRecord, setCanSaveRecord] = useState<boolean>(false)
 	const [error, setError] = useState<string | null>('')
 	const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
+	const [isSaving, setIsSaving] = useState<boolean>(false)
+
+	const { toast } = useToast()
 
 	const changeModal = () => {
 		setModalIsOpen(!modalIsOpen)
@@ -49,10 +53,16 @@ const StudentRecordDialog = () => {
 		if (!response.ok) {
 			setError('Não foi possível salvar o histórico')
 		} else {
-			setModalIsOpen(false)
+			setIsSaving(true)
 			setSelectedRecord(null)
+
 			const studentResponse = await response.json()
-			await revalidateRecordPage(getUsername(studentResponse.email))
+			revalidateRecordPage(getUsername(studentResponse.email)).then(() => {
+				setIsSaving(false)
+				setModalIsOpen(false)
+				setPdfViewerKey(Math.random())
+				toast({description: "Histórico atualizado."})
+			})
 		}
 
 		setCanSaveRecord(true)
@@ -72,46 +82,57 @@ const StudentRecordDialog = () => {
 					)}
         </DialogHeader>
 				<div className="space-y-2">
-                <div className="text-muted-foreground text-sm">
-                  Insira seu histórico acadêmico em formato PDF, com tamanho
-                  máximo de 5MB.
-                </div>
-                <div className="flex items-center justify-normal gap-x-2">
-                  <Button size="lg" type="button">
-                    <input
-                      type="file"
-                      className="hidden"
-                      id="fileInput"
-                      onChange={(e) => {
-                        setSelectedRecord(e.target.files?.[0] || null)
-                        setCanSaveRecord(!!e.target.files?.[0])
-                      }}
-                      accept="application/pdf"
-                    />
-                    <label
-                      htmlFor="fileInput"
-                      className="text-neutral-90  rounded-md cursor-pointer inline-flex items-center"
-                    >
-                      <Upload className="mr-2" />
-                      <span className="whitespace-nowrap">Fazer Upload</span>
-                    </label>
-                  </Button>
-                </div>
-              </div>
-							<DialogFooter>
-								<DialogClose asChild>
-									<Button type="button" variant="secondary">
-										Fechar
-									</Button>
-								</DialogClose>
-								<Button
-									type="submit"
-									onClick={submitHandler}
-									disabled={canSaveRecord === false}
-								>
-									Salvar
-								</Button>
-							</DialogFooter>
+					<div className="text-muted-foreground text-sm">
+						Insira seu histórico acadêmico em formato PDF, com tamanho
+						máximo de 5MB.
+					</div>
+					<div className="flex items-center justify-normal gap-x-2">
+						<Button size="lg" type="button">
+							<input
+								type="file"
+								className="hidden"
+								id="fileInput"
+								onChange={(e) => {
+									setSelectedRecord(e.target.files?.[0] || null)
+									setCanSaveRecord(!!e.target.files?.[0])
+								}}
+								accept="application/pdf"
+							/>
+							<label
+								htmlFor="fileInput"
+								className="text-neutral-90  rounded-md cursor-pointer inline-flex items-center"
+							>
+								<Upload className="mr-2" />
+								<span className="whitespace-nowrap">Fazer Upload</span>
+							</label>
+						</Button>
+					</div>
+					<div>
+						{ selectedRecord && 
+						<span className='text-muted-foreground text-sm'>Arquivo enviado: {selectedRecord.name}</span> 
+						}
+					</div>
+					<div>
+						{
+							isSaving &&
+							<span className='text-muted-foreground text-sm'>Salvando no sistema, aguarde</span>
+						}
+					</div>
+				</div>
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button type="button" variant="secondary">
+							Fechar
+						</Button>
+					</DialogClose>
+					<Button
+						type="submit"
+						onClick={() => { submitHandler() }}
+						disabled={canSaveRecord === false}
+					>
+						Salvar
+					</Button>
+				</DialogFooter>
       </DialogContent>
     </Dialog>
   )
